34c6ecac2810a44a134ef51a3cc357ee0aaf8974
Hamid Zandi <hamid.zandi.dev@gmail.com>
2025-09-03 19:40:00 +0330

    add get article list usecase

---
+++ lib/features/news_list/domain/usecase/get_articles_usecase.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:tech_news/core/error_handling/failure.dart';
+import 'package:tech_news/core/usecase/use_case.dart';
+import 'package:tech_news/features/news_list/domain/model/articles_model.dart';
+import 'package:tech_news/features/news_list/domain/repository/articles_repository.dart';
+
+class GetArticlesUsecase extends UseCase<Either<Failure, ArticlesModel>, ArticlesParams> {
+  final ArticlesRepository _articlesRepository;
+  GetArticlesUsecase(this._articlesRepository);
+
+  @override
+  Future<Either<Failure, ArticlesModel>> call(ArticlesParams input) {
+    return _articlesRepository.getArticles(input.page);
+  }
+}
+
+class ArticlesParams {
+  final int page;
+  ArticlesParams._(this.page);
+
+  static ArticlesParams forQuery(int page) {
+    return ArticlesParams._(page);
+  }
+}
